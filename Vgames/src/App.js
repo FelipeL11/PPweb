9b25aa3819d5a093c45a0e2cd2ff1a2fc6a34717
FelipeL11 <carlosslt88@gmail.com>
2024-08-22 23:02:55 -0500

    se le cambio el nombre a la carpeta del front

---
+++ Vgames/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Componente from './Micomponente';
+import './App.css'
+function App(){
+  return(
+    <div className="App"> 
+    
+    <Componente/>
+    </div>
+
+  );
+}
+
+export default App;
